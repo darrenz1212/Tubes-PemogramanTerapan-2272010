4ac8acffc678721ed81acb29b53a0cf53a2687c7
darrenz1212 <113487535+darrenz1212@users.noreply.github.com>
2024-06-03 13:13:12 +0700

    pengajuan done

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ from app.mahasiswaController import mahasiswaBp
 from app.userController import userBp
 from app.auth.login import loginBp
 from app.auth.register import registerBp
+from app.pengajuan.pengajuanController import pengajuanBp
 from dbConfig import DATABASE_URL
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Tambahkan secret key untuk session
+app.secret_key = 'your_secret_key' 
+app.config['UPLOAD_FOLDER'] = 'dokumenPengajuan'
 
 # Initialize SQLAlchemy
 engine = create_engine(DATABASE_URL)
@@ -24,6 +26,7 @@ app.register_blueprint(mahasiswaBp, url_prefix='/mahasiswa')
 app.register_blueprint(userBp, url_prefix='/user')
 app.register_blueprint(loginBp, url_prefix='/auth')
 app.register_blueprint(registerBp, url_prefix='/auth')
+app.register_blueprint(pengajuanBp, url_prefix='/pengajuan')
 
 if __name__ == '__main__':
     app.run(debug=True)
